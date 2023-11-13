54980efb9a3db27015142b0bdff146e32531bee7
John Doe <hugosum.dev@protonmail.com>
2023-11-13 08:35:30 +0000

    fix build

--- home-manager/android/mod.nix
+++ home-manager/android/mod.nix
@@ -1,6 +1,4 @@
 { inputs, lib, config, pkgs, ... }: {
-  imports = [ ./lsp.nix ./formatter.nix ];
-
   home.packages = with pkgs; [ ];
 
   home.sessionVariables = { ANDROID_HOME = "$HOME/Library/Android/sdk"; };
