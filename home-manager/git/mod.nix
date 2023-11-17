0662ff9bcee3a517d37eb53cdd85c6fb12687f3e
John Doe <hugosum.dev@protonmail.com>
2023-11-17 10:21:59 +0000

    no ff

--- home-manager/git/mod.nix
+++ home-manager/git/mod.nix
@@ -3,7 +3,7 @@
   programs.git.delta.enable = true;
 
   programs.git.extraConfig = {
-    pull = { ff = "only"; };
+    pull = { ff = false; };
     # REF https://stackoverflow.com/a/61920529
     http = { postBuffer = 524288000; };
 
