95e7e626a9260ed5a004b1669af8938a32a37bb7
John Doe <hugosum.dev@protonmail.com>
2024-12-21 11:27:51 +0000

    update

--- home-manager/modules/docker/mod.nix
+++ home-manager/modules/docker/mod.nix
@@ -1,8 +1,9 @@
-{ inputs, lib, config, pkgs, ... }: {
-  services.podman = { enable = true; };
-  # FIXME enable this once we have upgrade to 25.05
-  # services.podman.settings.policy = { };
-  home.packages = with pkgs; [ podman ];
-
-  home.shellAliases = { docker = "podman"; };
+{ inputs, lib, config, pkgs, ... }:
+{
+  # # FIXME enable this once we have upgrade to 25.05, as this option does not exist in home-manager yet, and this module is failing to run
+  # services.podman = { enable = true; };
+  # # services.podman.settings.policy = { };
+  # home.packages = with pkgs; [ podman ];
+  #
+  # home.shellAliases = { docker = "podman"; };
 }
