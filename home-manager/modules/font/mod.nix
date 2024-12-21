18d56ed2e254277dae08d45b5e15d2767e0552f8
John Doe <hugosum.dev@protonmail.com>
2024-12-21 10:10:55 +0000

    add more firefox related settings

--- home-manager/modules/font/mod.nix
+++ home-manager/modules/font/mod.nix
@@ -1,6 +1,8 @@
 { inputs, lib, config, pkgs, ... }: {
   home.packages = with pkgs; [
-    nerdfonts
+    # NOTE only use this for testing, the default nerdfont set is too large
+    (nerdfonts.override { fonts = [ "0xProto" "Iosevka" ]; })
+
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
   ];
@@ -9,7 +11,7 @@
     defaultFonts = {
       monospace = [ "0xProto Nerd Font Mono" "Noto Sans Mono CJK HK" ];
       serif = [ "Noto Serif CJK HK" ];
-      sansSerif = [ "0xProto Nerd Font" "Noto Sans CJK HK" ];
+      sansSerif = [ "Noto Sans CJK HK" ];
     };
   };
 }
--- home-manager/modules/zsh/mod.nix
+++ home-manager/modules/zsh/mod.nix
@@ -1,4 +1,4 @@
-{ inputs, lib, config, pkgs, ... }: {
+{ inputs, lib, config, pkgs, system, ... }: {
   home.packages = with pkgs; [ neofetch ];
   programs.zsh = {
     enable = true;
@@ -21,19 +21,25 @@
         }
       ];
     };
-    #       # for navigating prev and next command in history
-    #       bindkey '\u00AB' up-line-or-history;
-    #       bindkey '\u00AC' down-line-or-history;
-    # ^N^P
-    #       # this has to match the keybinding of Wezterm, this corresponding to <Char-0xAA>. Cannot use 0x here
-    #       bindkey '\u00AA' vi-cmd-mode;
-    #       # REF https://unix.stackexchange.com/a/290403
-    #       bindkey -v '^?' backward-delete-char;
-    initExtra = ''
+    initExtra = if lib.strings.hasInfix "linux" system then ''
       source "$HOME/.config/fzf/fzf-color.sh"
       bindkey '^P' up-line-or-history;
       bindkey '^N' down-line-or-history;
 
+      KEYTIMEOUT=1;
+      unsetopt share_history;
+    '' else ''
+      source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh;
+      source /nix/var/nix/profiles/default/etc/profile.d/nix.sh;
+
+      source "$HOME/.config/fzf/fzf-color.sh"
+      # for navigating prev and next command in history
+      bindkey '\u00AB' up-line-or-history;
+      bindkey '\u00AC' down-line-or-history;
+      # this has to match the keybinding of Wezterm, this corresponding to <Char-0xAA>. Cannot use 0x here
+      bindkey '\u00AA' vi-cmd-mode;
+      # REF https://unix.stackexchange.com/a/290403
+      bindkey -v '^?' backward-delete-char;
       KEYTIMEOUT=1;
       unsetopt share_history;
     '';
