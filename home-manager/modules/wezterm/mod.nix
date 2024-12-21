d5d9388c0b4f64c12a95611547966ad67955baff
John Doe <hugosum.dev@protonmail.com>
2024-12-21 20:00:08 +0000

    pin verison

--- home-manager/modules/wezterm/mod.nix
+++ home-manager/modules/wezterm/mod.nix
@@ -1,12 +1,12 @@
-{ inputs, lib, config, pkgs, system, ... }: {
+{ inputs, lib, config, pkgs, system, isDarwin, ... }: {
   home.packages = with pkgs; [ wezterm ];
 
   xdg.configFile = {
     "wezterm/wezterm.lua" = {
-      source = if lib.strings.hasInfix "linux" system then
-        ./linux.lua
+      source = if isDarwin then
+        ./darwin.lua
       else
-        ./darwin.lua;
+        ./linux.lua;
     };
     "wezterm/common.lua" = { source = ./common.lua; };
   };
