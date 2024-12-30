a1971be296ff7fdf0a3b69e2469c7b90929093eb
nobody <hugosum.dev@protonmail.com>
2024-12-30 18:21:33 +0000

    add keybinding

--- home-manager/modules/wezterm/darwin.lua
+++ home-manager/modules/wezterm/darwin.lua
@@ -3,7 +3,10 @@ local common = require("common")
 
 local config = common.config
 
-local darwinOnlyMapping = {}
+local darwinOnlyMapping = {
+	{ key = "c", mods = "CMD", action = wezterm.action({ CopyTo = "Clipboard" }) },
+	{ key = "v", mods = "CMD", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+}
 
 for _, mapping in ipairs(darwinOnlyMapping) do
 	table.insert(config.keys, mapping)
