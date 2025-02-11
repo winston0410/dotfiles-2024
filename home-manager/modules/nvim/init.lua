ec01b17d053f6c536504892d90861a513a6de84e
nobody <hugosum.dev@protonmail.com>
2025-02-11 21:23:47 +0000

    progress

--- home-manager/modules/nvim/init.lua
+++ home-manager/modules/nvim/init.lua
@@ -943,7 +943,54 @@ require("lazy").setup({
 					desc = "Open kubectl.nvim panel",
 				},
 			},
-			opts = {},
+			config = function()
+				require("kubectl").setup({
+					log_level = vim.log.levels.INFO,
+					auto_refresh = {
+						enabled = true,
+						interval = 300,
+					},
+					diff = {
+						bin = "kubediff",
+					},
+					kubectl_cmd = { cmd = "kubectl", env = {}, args = {}, persist_context_change = false },
+					terminal_cmd = nil,
+					namespace = "All",
+					namespace_fallback = {},
+					hints = true,
+					context = true,
+					heartbeat = true,
+					lineage = {
+						enabled = false,
+					},
+					logs = {
+						prefix = true,
+						timestamps = true,
+						since = "5m",
+					},
+					alias = {
+						apply_on_select_from_history = true,
+						max_history = 5,
+					},
+					filter = {
+						apply_on_select_from_history = true,
+						max_history = 10,
+					},
+					float_size = {
+						width = 0.9,
+						height = 0.8,
+						col = 10,
+						row = 5,
+					},
+					obj_fresh = 5,
+					skew = {
+						enabled = true,
+						log_level = vim.log.levels.INFO,
+					},
+					headers = true,
+					completion = { follow_cursor = true },
+				})
+			end,
 		},
 		{
 			"mistweaverco/kulala.nvim",
