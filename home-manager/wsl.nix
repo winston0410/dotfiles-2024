a265ca46d0482045c4f082567f9ccad02f3ae77e
nobody <hugosum.dev@protonmail.com>
2025-02-18 22:00:01 +0000

    add yank for wsl

--- home-manager/wsl.nix
+++ home-manager/wsl.nix
@@ -1,6 +1,11 @@
 { inputs, lib, config, pkgs, ... }: {
   imports = [ ./common.nix ];
 
+  home.packages = with pkgs;
+    [
+      # No clipboard provider in WSL
+      yank
+    ];
   home = {
     username = "hugosum";
     homeDirectory = "/home/hugosum";
