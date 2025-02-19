c2eeb4efc38a51bc66e537a2c86e25eb24aa3df6
nobody <hugosum.dev@protonmail.com>
2025-02-19 10:44:13 +0000

    add font

--- home-manager/wsl.nix
+++ home-manager/wsl.nix
@@ -1,11 +1,11 @@
 { inputs, lib, config, pkgs, ... }: {
   imports = [ ./common.nix ];
 
-  home.packages = with pkgs;
-    [
-      # No clipboard provider in WSL
-      yank
-    ];
+  home.packages = with pkgs; [
+    # No clipboard provider in WSL
+    yank
+    (nerdfonts.override { fonts = [ "0xProto" ]; })
+  ];
   home = {
     username = "hugosum";
     homeDirectory = "/home/hugosum";
