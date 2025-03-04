e4e1acea25d28eaf8a9cec0326c2007aaa2cbbe7
nobody <hugosum.dev@protonmail.com>
2025-03-04 00:35:31 +0000

    progress

--- home-manager/modules/nvim/init.lua
+++ home-manager/modules/nvim/init.lua
@@ -3649,7 +3649,20 @@ require("lazy").setup({
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 	callback = function(ev)
-		local supported_modes = { "n", "v" }
+		local supported_modes = { "n" }
+		vim.keymap.set(supported_modes, "]de", function()
+			vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+		end, { silent = true, noremap = true, buffer = ev.buf, desc = "Jump to next error" })
+		vim.keymap.set(supported_modes, "[de", function()
+			vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+		end, { silent = true, noremap = true, buffer = ev.buf, desc = "Jump to previous error" })
+
+		vim.keymap.set(supported_modes, "]dw", function()
+			vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
+		end, { silent = true, noremap = true, buffer = ev.buf, desc = "Jump to next warning" })
+		vim.keymap.set(supported_modes, "[dw", function()
+			vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
+		end, { silent = true, noremap = true, buffer = ev.buf, desc = "Jump to previous warning" })
 		vim.keymap.set(supported_modes, "<leader>ss", function()
 			-- if we call twice, we will enter the hover windows immediately after running the keybinding
 			vim.lsp.buf.hover()
@@ -3724,46 +3737,3 @@ vim.api.nvim_create_autocmd("LspProgress", {
 		})
 	end,
 })
--- ---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
--- local progress = vim.defaulttable()
--- vim.api.nvim_create_autocmd("LspProgress", {
--- 	---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
--- 	callback = function(ev)
--- 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
--- 		local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
--- 		if not client or type(value) ~= "table" then
--- 			return
--- 		end
--- 		local p = progress[client.id]
---
--- 		for i = 1, #p + 1 do
--- 			if i == #p + 1 or p[i].token == ev.data.params.token then
--- 				p[i] = {
--- 					token = ev.data.params.token,
--- 					msg = ("[%3d%%] %s%s"):format(
--- 						value.kind == "end" and 100 or value.percentage or 100,
--- 						value.title or "",
--- 						value.message and (" **%s**"):format(value.message) or ""
--- 					),
--- 					done = value.kind == "end",
--- 				}
--- 				break
--- 			end
--- 		end
---
--- 		local msg = {} ---@type string[]
--- 		progress[client.id] = vim.tbl_filter(function(v)
--- 			return table.insert(msg, v.msg) or not v.done
--- 		end, p)
---
--- 		local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
--- 		vim.notify(table.concat(msg, "\n"), "info", {
--- 			id = "lsp_progress",
--- 			title = client.name,
--- 			opts = function(notif)
--- 				notif.icon = #progress[client.id] == 0 and " "
--- 					or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
--- 			end,
--- 		})
--- 	end,
--- })
