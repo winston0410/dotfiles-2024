7ca1f3fa2c183c2ee7fa95b6630390dad4d1bab3
nobody <hugosum.dev@protonmail.com>
2025-03-19 08:53:44 +0000

    create minimal config

--- home-manager/modules/nvim/minimal.lua
+++ home-manager/modules/nvim/minimal.lua
@@ -22,6 +22,233 @@ require("lazy").setup({
 		hererocks = false,
 	},
 	spec = {
-		unpack(essential.packages),
+		{
+			"rose-pine/neovim",
+			name = "rose-pine",
+			lazy = true,
+			init = function()
+				vim.opt.wildignore:append({
+					"rose-pine.lua",
+					"rose-pine-dawn.lua",
+				})
+				vim.cmd.colorscheme("rose-pine-moon")
+			end,
+		},
+		{
+			"nvim-lualine/lualine.nvim",
+			event = { "VeryLazy" },
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			config = function()
+				local should_show_buffers = function()
+					local tab_id = vim.api.nvim_get_current_tabpage()
+					local ok, lockbuffer = pcall(function()
+						return vim.api.nvim_tabpage_get_var(tab_id, "lockbuffer")
+					end)
+
+					if ok then
+						return not lockbuffer
+					end
+
+					return true
+				end
+				local utility_filetypes = {
+					"terminal",
+					"snacks_terminal",
+					"oil",
+					"trouble",
+					-- "qf",
+					"DiffviewFileHistory",
+					"DiffviewFiles",
+					"snacks_dashboard",
+					"snacks_picker_list",
+					"snacks_layout_box",
+					"snacks_picker_input",
+					"snacks_win_backdrop",
+					"NeogitStatus",
+					"NeogitCommitView",
+					"NeogitStashView",
+					"NeogitConsole",
+					"NeogitLogView",
+					"NeogitDiffView",
+				}
+				require("lualine").setup({
+					options = {
+						theme = "auto",
+						component_separators = "",
+						section_separators = "",
+						disabled_filetypes = {
+							winbar = utility_filetypes,
+							inactive_winbar = utility_filetypes,
+						},
+						always_show_tabline = true,
+						globalstatus = true,
+					},
+					tabline = {
+						lualine_a = {},
+						lualine_b = {},
+						lualine_c = {
+							{
+								"buffers",
+								mode = 0,
+								icons_enabled = true,
+								max_length = function()
+									return vim.o.columns / 2
+								end,
+								filetype_names = {
+									checkhealth = "Healthcheck",
+									qf = "Quickfix",
+								},
+								symbols = {
+									modified = "[+]",
+									alternate_file = "",
+								},
+								cond = should_show_buffers,
+							},
+						},
+						lualine_x = {
+							{
+								"tabs",
+								mode = 0,
+								max_length = function()
+									return vim.o.columns / 2
+								end,
+							},
+						},
+						lualine_y = {},
+						lualine_z = {},
+					},
+					winbar = {
+						lualine_a = {},
+						lualine_b = {},
+						lualine_c = {},
+						lualine_x = {},
+						lualine_y = {},
+						lualine_z = {},
+					},
+					inactive_winbar = {
+						lualine_a = {},
+						lualine_b = {},
+						lualine_c = {},
+						lualine_x = {},
+						lualine_y = {},
+						lualine_z = {},
+					},
+					inactive_sections = {
+						lualine_a = {},
+						lualine_b = {},
+						lualine_c = {},
+						lualine_x = {},
+						lualine_y = {},
+						lualine_z = {},
+					},
+					sections = {
+						lualine_a = { "mode" },
+						lualine_b = {
+							"branch",
+						},
+						lualine_c = {
+							{
+								"location",
+								cond = function()
+									return not vim.list_contains(utility_filetypes, vim.bo.filetype)
+								end,
+								padding = { left = 1, right = 0 },
+							},
+							{
+								"encoding",
+								cond = function()
+									return not vim.list_contains(utility_filetypes, vim.bo.filetype)
+								end,
+								padding = { left = 1, right = 1 },
+							},
+							{
+								"filesize",
+								cond = function()
+									return not vim.list_contains(utility_filetypes, vim.bo.filetype)
+								end,
+								padding = { left = 1, right = 1 },
+							},
+							"%=",
+							{
+								function()
+									local current_tab = vim.api.nvim_get_current_tabpage()
+
+									local tab_number = vim.api.nvim_tabpage_get_number(current_tab)
+									local cwd = vim.fn.getcwd(-1, tab_number)
+									local home = os.getenv("HOME")
+									if home then
+										cwd = cwd:gsub("^" .. home, "~")
+									end
+									return cwd
+								end,
+							},
+							"%=",
+						},
+						lualine_x = {},
+						lualine_y = {},
+						lualine_z = {},
+					},
+				})
+			end,
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+		build = function()
+			vim.cmd("TSUpdate")
+		end,
+		lazy = false,
+		priority = 999,
+		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+		config = function()
+			local installer = require("nvim-treesitter.install")
+			installer.prefer_git = true
+
+			local config = {
+				ensure_installed = "all",
+				auto_install = false,
+				sync_install = false,
+				ignore_install = {},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = false,
+						node_incremental = "+",
+						node_decremental = "-",
+						scope_incremental = false,
+					},
+				},
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+					priority = {
+						["@comment.error"] = 999,
+						["@comment.warning"] = 999,
+						["@comment.note"] = 999,
+						["@comment.todo"] = 999,
+						-- ["@comment.info"] = 999,
+						-- ["@comment.hint"] = 999,
+					},
+				},
+				indent = { enable = true },
+				query_linter = {
+					enable = true,
+					use_virtual_text = true,
+					lint_events = { "BufWrite", "CursorHold" },
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+					},
+				},
+			}
+			require("nvim-treesitter.configs").setup(config)
+		end,
 	},
 })
