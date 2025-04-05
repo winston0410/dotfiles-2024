ba4718281a07dc9fb7accd458ce44719efa569d8
nobody <hugosum.dev@protonmail.com>
2025-04-05 08:49:08 +0100

    fix lua-lsp

--- home-manager/modules/nvim/lsp/lua_ls.lua
+++ home-manager/modules/nvim/lsp/lua_ls.lua
@@ -1,33 +1,5 @@
 return {
-	diagnostics = {
-		underline = true,
-		update_in_insert = true,
-		severity_sort = true,
-	},
-	on_init = function(client)
-		-- FIXME seems to be able to prevent LSP from highlighting
-		client.server_capabilities.semanticTokensProvider = nil
-
-		if client.workspace_folders then
-			local path = client.workspace_folders[1].name
-			if
-				path ~= vim.fn.stdpath("config")
-				and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc"))
-			then
-				return
-			end
-		end
-
-		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-			runtime = {
-				version = "LuaJIT",
-			},
-			workspace = {
-				checkThirdParty = false,
-				library = {},
-			},
-		})
-	end,
+	root_markers = { ".luarc.json", ".luarc.jsonc" },
 	settings = {
 		Lua = {
 			diagnostics = {
