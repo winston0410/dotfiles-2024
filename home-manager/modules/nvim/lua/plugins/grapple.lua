acc0dca05e41a81ff3458abbe4e33566f01ffefa
nobody <hugosum.dev@protonmail.com>
2025-07-02 00:01:02 +0100

    progress

--- home-manager/modules/nvim/lua/plugins/grapple.lua
+++ home-manager/modules/nvim/lua/plugins/grapple.lua
@@ -1,5 +1,6 @@
 return {
 	{
+		-- TODO implement position specific tag https://github.com/cbochs/grapple.nvim/issues/118
 		"cbochs/grapple.nvim",
 		version = "0.30.x",
 		dependencies = {
@@ -8,19 +9,17 @@ return {
 		config = function()
 			-- TODO wait for tag_hook to be documented, and we can override it to change keymapping inside tag windows
 			require("grapple").setup({
+				default_scopes = {
+					global = { hidden = true },
+					lsp = { hidden = true },
+					static = { hidden = true },
+				},
 				scope = "git_branch",
 			})
 		end,
 		event = { "BufReadPost", "BufNewFile" },
 		cmd = "Grapple",
 		keys = {
-			-- {
-			-- 	"<leader>mm",
-			-- 	function()
-			-- 		require("grapple").toggle({})
-			-- 	end,
-			-- 	desc = "Grapple toggle tag",
-			-- },
 			{
 				"<leader>p<leader>m",
 				function()
@@ -55,7 +54,11 @@ return {
 						if input == nil then
 							return
 						end
-						require("grapple").tag({ name = input })
+
+						local err = require("grapple").tag({ name = input })
+						if err ~= nil then
+							return
+						end
 						vim.notify("Grapple tag created", vim.log.levels.INFO)
 					end)
 				end,
@@ -66,13 +69,90 @@ return {
 			{
 				"<leader>mq",
 				function()
-					require("grapple").untag()
+					local err = require("grapple").untag()
+					if err ~= nil then
+						return
+					end
 					vim.notify("Grapple tag removed", vim.log.levels.INFO)
 				end,
 				silent = true,
 				noremap = true,
 				desc = "Grapple delete tag",
 			},
+			{
+				"<leader>mQ",
+				function()
+					local err = require("grapple").reset({ notify = true })
+					if err ~= nil then
+						return
+					end
+				end,
+				silent = true,
+				noremap = true,
+				desc = "Grapple remove all tags in a scope",
+			},
+			{
+				"<leader>m<leader>k",
+				function()
+					require("grapple").quickfix()
+				end,
+				silent = true,
+				noremap = true,
+				desc = "Grapple loads tag to quickfix",
+			},
+			{
+				"<leader>mso",
+				function()
+					require("grapple").cycle_scopes("next")
+				end,
+				silent = true,
+				noremap = true,
+				desc = "Grapple cycle next scope",
+			},
+			{
+				"<leader>msi",
+				function()
+					require("grapple").cycle_scopes("prev")
+				end,
+				silent = true,
+				noremap = true,
+				desc = "Grapple cycle prev scope",
+			},
+			-- NOTE don't see a need to create scope right now
+			-- {
+			-- 	"<leader>msv",
+			-- 	function()
+			-- 		vim.ui.input({ prompt = "Grapple scope name" }, function(input)
+			-- 			if input == nil then
+			-- 				return
+			-- 			end
+			--
+			-- 			local err = require("grapple").define_scope({
+			-- 				name = input,
+			-- 				-- REF https://github.com/cbochs/grapple.nvim/blob/b41ddfc1c39f87f3d1799b99c2f0f1daa524c5f7/lua/grapple/settings.lua#L94
+			-- 				resolver = function()
+			-- 					return vim.loop.cwd(), vim.loop.cwd()
+			-- 				end,
+			-- 			})
+			-- 			if err ~= nil then
+			-- 				return
+			-- 			end
+			-- 			vim.notify("Grapple scope created", vim.log.levels.INFO)
+			-- 		end)
+			-- 	end,
+			-- 	silent = true,
+			-- 	noremap = true,
+			-- 	desc = "Create a Grapple scope",
+			-- },
+			-- {
+			-- 	"<leader>msd",
+			-- 	function()
+			-- 		-- require("grapple").delete_scope()
+			-- 	end,
+			-- 	silent = true,
+			-- 	noremap = true,
+			-- 	desc = "Delete a Grapple scope",
+			-- },
 		},
 	},
 }
