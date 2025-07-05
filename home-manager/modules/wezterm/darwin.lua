2da9c938338157321e617b9d868d74c5fc975ebb
nobody <hugosum.dev@protonmail.com>
2025-07-05 21:17:29 +0100

    use native fullscreen mode

--- home-manager/modules/wezterm/darwin.lua
+++ home-manager/modules/wezterm/darwin.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 local common = require("common")
 
 local config = common.config
+config.native_macos_fullscreen_mode = true
 
 local darwinOnlyMapping = {
 	{ key = "c", mods = "CMD", action = wezterm.action({ CopyTo = "Clipboard" }) },
@@ -13,18 +14,5 @@ for _, mapping in ipairs(darwinOnlyMapping) do
 end
 
 -- REF https://github.com/wezterm/wezterm/issues/3299#issuecomment-2145712082
-wezterm.on("gui-startup", function(cmd)
-	local active = wezterm.gui.screens().active
-
-	local _, _, window = wezterm.mux.spawn_window(cmd or {
-		x = active.x,
-		y = active.y,
-		width = active.width,
-		height = active.height,
-	})
-
-	window:gui_window():set_position(active.x, active.y)
-	window:gui_window():set_inner_size(active.width, active.height)
-end)
 
 return config
