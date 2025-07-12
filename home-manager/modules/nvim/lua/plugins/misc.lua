2a17771bdff4a4018d378cf0b67f0139c04d4d52
nobody <hugosum.dev@protonmail.com>
2025-07-12 19:41:27 +0100

    new plugin

--- home-manager/modules/nvim/lua/plugins/misc.lua
+++ home-manager/modules/nvim/lua/plugins/misc.lua
@@ -6,4 +6,57 @@ return {
 			supported_file_formats = { "*.yaml", "*.json", "*.ini" },
 		},
 	},
+	{
+		"michaelb/sniprun",
+		version = "1.x",
+		build = false,
+		cmd = { "SnipClose", "SnipInfo", "SnipReset", "SnipRun", "SnipReplMemoryClean" },
+		config = function()
+			require("sniprun").setup({
+				binary_path = "sniprun",
+			})
+		end,
+	},
+	{
+		"stevearc/overseer.nvim",
+		cmd = {
+			"OverseerOpen",
+			"OverseerClose",
+			"OverseerToggle",
+			"OverseerSaveBundle",
+			"OverseerLoadBundle",
+			"OverseerDeleteBundle",
+			"OverseerRunCmd",
+			"OverseerRun",
+			"OverseerInfo",
+			"OverseerBuild",
+			"OverseerQuickAction",
+			"OverseerTaskAction",
+			"OverseerClearCache",
+		},
+		keys = {
+			{
+				"<leader>e",
+				function()
+					require("overseer").toggle()
+				end,
+				mode = { "n" },
+				silent = true,
+				noremap = true,
+				desc = "Toggle Overseer",
+			},
+			{
+				"<leader>p<leader>e",
+				function()
+					vim.cmd("OverseerRun")
+				end,
+				mode = { "n" },
+				silent = true,
+				noremap = true,
+				desc = "Pick task to run",
+			},
+		},
+		version = "1.x",
+		opts = {},
+	},
 }
