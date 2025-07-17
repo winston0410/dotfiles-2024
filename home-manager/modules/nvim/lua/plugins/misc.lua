af4742d373d83a4d9a024872fa4d2ac3662cfeb1
nobody <hugosum.dev@protonmail.com>
2025-07-17 10:42:18 +0100

    feat(nvim): Add visual-whitespace.nvim plugin
    Adds the `visual-whitespace.nvim` plugin to improve visibility of whitespace characters.

--- home-manager/modules/nvim/lua/plugins/misc.lua
+++ home-manager/modules/nvim/lua/plugins/misc.lua
@@ -7,6 +7,17 @@ return {
 		opts = {},
 	},
 	{
+		"mcauley-penney/visual-whitespace.nvim",
+		event = "VeryLazy",
+		config = function()
+			local comment_hl = vim.api.nvim_get_hl(0, { name = "@comment", link = false })
+			local visual_hl = vim.api.nvim_get_hl(0, { name = "Visual", link = false })
+			require("visual-whitespace").setup({
+				highlight = { fg = comment_hl.fg, bg = visual_hl.bg },
+			})
+		end,
+	},
+	{
 		"trixnz/sops.nvim",
 		lazy = false,
 		opts = {
