30a5f49be9bca216301b2c13ce37eb7ecbe96e33
nobody <hugosum.dev@protonmail.com>
2025-08-24 20:27:20 +0100

    progress

--- home-manager/modules/nvim/lua/plugins/neovim-as-platform.lua
+++ home-manager/modules/nvim/lua/plugins/neovim-as-platform.lua
@@ -28,8 +28,9 @@ return {
 	},
 	{
 		"ramilito/kubectl.nvim",
-		version = "2.x",
+		version = "2.7.12",
 		cmd = { "Kubectl", "Kubectx", "Kubens" },
+		dependencies = { "saghen/blink.download" },
 		keys = {
 			{
 				"<leader>K",
@@ -42,55 +43,12 @@ return {
 				desc = "kubectl.nvim panel",
 			},
 		},
-		dependencies = { "saghen/blink.download" },
 		config = function()
 			require("kubectl").setup({
 				log_level = vim.log.levels.INFO,
-				auto_refresh = {
-					enabled = true,
-					interval = 300,
-				},
 				diff = {
 					bin = "kubediff",
 				},
-				kubectl_cmd = { cmd = "kubectl", env = {}, args = {}, persist_context_change = false },
-				terminal_cmd = nil,
-				namespace = "All",
-				namespace_fallback = {},
-				hints = true,
-				context = true,
-				heartbeat = true,
-				lineage = {
-					enabled = false,
-				},
-				logs = {
-					prefix = true,
-					timestamps = true,
-					since = "5m",
-				},
-				alias = {
-					apply_on_select_from_history = true,
-					max_history = 5,
-				},
-				filter = {
-					apply_on_select_from_history = true,
-					max_history = 10,
-				},
-				float_size = {
-					width = 0.9,
-					height = 0.8,
-					col = 10,
-					row = 5,
-				},
-				obj_fresh = 5,
-				skew = {
-					enabled = true,
-					log_level = vim.log.levels.INFO,
-				},
-				headers = {
-					enabled = false,
-				},
-				completion = { follow_cursor = true },
 			})
 			local group = vim.api.nvim_create_augroup("kubectl_mappings", { clear = true })
 			vim.api.nvim_create_autocmd("FileType", {
