d29db7d4d217a9ba168e651bc1cb7510d94eaf49
Hugo Sum <hsum@trintech.com>
2025-09-09 14:27:00 +0100

    clean up

--- home-manager/modules/nvim/lua/custom/essential.lua
+++ home-manager/modules/nvim/lua/custom/essential.lua
@@ -161,27 +161,28 @@ vim.keymap.set({ "n" }, "<leader>T", function()
 	vim.cmd("term")
 end, { remap = true, silent = true, desc = "Open terminal" })
 
-local clear_buffer_keybinding = "<leader>bc"
-local delete_buffer_keybinding = "<leader>bq"
-local next_buffer_keybinding = "<leader>bo"
-local prev_buffer_keybinding = "<leader>bi"
-vim.keymap.set({ "n" }, clear_buffer_keybinding, function()
-	-- TODO
-end, { silent = true, noremap = true, desc = "Unload other buffers" })
-vim.keymap.set({ "n" }, delete_buffer_keybinding, function()
-	Snacks.bufdelete.delete()
-end, { silent = true, noremap = true, desc = "Delete current buffer" })
-vim.keymap.set({ "n" }, next_buffer_keybinding, function()
-	vim.cmd("bnext")
-end, { silent = true, noremap = true, desc = "Go to next buffer" })
-vim.keymap.set({ "n" }, prev_buffer_keybinding, function()
-	vim.cmd("bprev")
-end, { silent = true, noremap = true, desc = "Go to previous buffer" })
-for i = 1, 9 do
-	vim.keymap.set({ "n" }, "<leader>b" .. i, function()
-		vim.cmd(string.format("LualineBuffersJump %s", i))
-	end, { noremap = true, silent = true, desc = string.format("Jump to buffer %s", i) })
-end
+-- NOTE buffer jumping is not really that useful
+-- local clear_buffer_keybinding = "<leader>bc"
+-- local delete_buffer_keybinding = "<leader>bq"
+-- local next_buffer_keybinding = "<leader>bo"
+-- local prev_buffer_keybinding = "<leader>bi"
+-- vim.keymap.set({ "n" }, clear_buffer_keybinding, function()
+-- 	-- TODO
+-- end, { silent = true, noremap = true, desc = "Unload other buffers" })
+-- vim.keymap.set({ "n" }, delete_buffer_keybinding, function()
+-- 	Snacks.bufdelete.delete()
+-- end, { silent = true, noremap = true, desc = "Delete current buffer" })
+-- vim.keymap.set({ "n" }, next_buffer_keybinding, function()
+-- 	vim.cmd("bnext")
+-- end, { silent = true, noremap = true, desc = "Go to next buffer" })
+-- vim.keymap.set({ "n" }, prev_buffer_keybinding, function()
+-- 	vim.cmd("bprev")
+-- end, { silent = true, noremap = true, desc = "Go to previous buffer" })
+-- for i = 1, 9 do
+-- 	vim.keymap.set({ "n" }, "<leader>b" .. i, function()
+-- 		vim.cmd(string.format("LualineBuffersJump %s", i))
+-- 	end, { noremap = true, silent = true, desc = string.format("Jump to buffer %s", i) })
+-- end
 
 vim.api.nvim_create_autocmd("BufEnter", {
 	pattern = "*",
