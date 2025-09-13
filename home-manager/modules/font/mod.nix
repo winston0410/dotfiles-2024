e7f5b271c0859cea7ad46326556650d29a92c4f3
nobody <hugosum.dev@protonmail.com>
2025-09-13 23:14:26 +0100

    add basic font

--- home-manager/modules/font/mod.nix
+++ home-manager/modules/font/mod.nix
@@ -1,13 +1,14 @@
 { inputs, lib, config, pkgs, ... }: {
   home.packages = with pkgs; [
     nerd-fonts._0xproto
+    dejavu_fonts
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
   ];
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
-      monospace = [ "0xProto Nerd Font Mono" "Noto Sans Mono CJK HK" ];
+      monospace = [ "0xProto Nerd Font Mono" "Noto Sans Mono CJK HK" "DejaVu Sans Mono" ];
       serif = [ "Noto Serif CJK HK" ];
       sansSerif = [ "Inter" "Noto Sans CJK HK" ];
     };
