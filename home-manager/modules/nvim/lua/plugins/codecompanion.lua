29b3711f8fd1e30cf50090d10797e921aaadee31
nobody <hugosum.dev@protonmail.com>
2025-10-04 13:58:59 +0100

    update

--- home-manager/modules/nvim/lua/plugins/codecompanion.lua
+++ home-manager/modules/nvim/lua/plugins/codecompanion.lua
@@ -144,11 +144,40 @@ return {
 				dependencies = {
 					"saghen/blink.cmp",
 				},
-				opts = {
-					preview = {
-						filetypes = { "markdown", "codecompanion", "md", "rmd", "quarto", "yaml", "typst"},
-					},
-				},
+				opts = function()
+					local function conceal_tag(icon, hl_group)
+						return {
+							on_node = { hl_group = hl_group },
+							on_closing_tag = { conceal = "" },
+							on_opening_tag = {
+								conceal = "",
+								virt_text_pos = "inline",
+								virt_text = { { icon .. " ", hl_group } },
+							},
+						}
+					end
+					return {
+						html = {
+							container_elements = {
+								["^buf$"] = conceal_tag("", "CodeCompanionChatVariable"),
+								["^file$"] = conceal_tag("", "CodeCompanionChatVariable"),
+								["^help$"] = conceal_tag("󰘥", "CodeCompanionChatVariable"),
+								["^image$"] = conceal_tag("", "CodeCompanionChatVariable"),
+								["^symbols$"] = conceal_tag("", "CodeCompanionChatVariable"),
+								["^url$"] = conceal_tag("󰖟", "CodeCompanionChatVariable"),
+								["^var$"] = conceal_tag("", "CodeCompanionChatVariable"),
+								["^tool$"] = conceal_tag("", "CodeCompanionChatTool"),
+								["^user_prompt$"] = conceal_tag("", "CodeCompanionChatTool"),
+								["^group$"] = conceal_tag("", "CodeCompanionChatToolGroup"),
+							},
+						},
+                        -- FIXME known issue, it is giving out an error when open CodeCompanionChat
+						preview = {
+							filetypes = { "markdown", "codecompanion", "md", "rmd", "quarto", "yaml", "typst" },
+							ignore_buftypes = {},
+						},
+					}
+				end,
 			},
 			{
 				"MeanderingProgrammer/render-markdown.nvim",
