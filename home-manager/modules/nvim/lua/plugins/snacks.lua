b6ce5db5b15798d56babbeeda8bf04c476c7fae1
nobody <hugosum.dev@protonmail.com>
2025-10-17 13:17:44 +0100

    clean up

--- home-manager/modules/nvim/lua/plugins/snacks.lua
+++ home-manager/modules/nvim/lua/plugins/snacks.lua
@@ -178,6 +178,7 @@ return {
 				function()
 					Snacks.picker.pick({
 						source = "enhanced_git_log",
+                        notify = false,
 						format = "git_log",
 						preview = "git_show",
 						confirm = "diffview",
@@ -447,6 +448,55 @@ return {
 			end, {
 				desc = "Reduce distraction",
 			})
+
+
+            vim.keymap.set('n', '<space>sg', function()
+  Snacks.picker.pick {
+    format = 'file',
+    notify = false, -- Also prevents error when searching with additional arguments
+    show_empty = true,
+    live = true,
+    supports_live = true,
+    -- hidden = true,
+    -- ignored = true,
+    ---@param opts snacks.picker.grep.Config
+    finder = function(opts, ctx)
+      local cmd = 'ast-grep'
+      local args = { 'run', '--color=never', '--json=stream' }
+      if vim.fn.has 'win32' == 1 then
+        cmd = 'sg'
+      end
+      if opts.hidden then
+        table.insert(args, '--no-ignore=hidden')
+      end
+      if opts.ignored then
+        table.insert(args, '--no-ignore=vcs')
+      end
+      local pattern, pargs = Snacks.picker.util.parse(ctx.filter.search)
+      table.insert(args, string.format('--pattern=%s', pattern))
+      vim.list_extend(args, pargs)
+      return require('snacks.picker.source.proc').proc({
+        opts,
+        {
+          cmd = cmd,
+          args = args,
+          transform = function(item)
+            local entry = vim.json.decode(item.text)
+            if vim.tbl_isempty(entry) then
+              return false
+            else
+              local start = entry.range.start
+              item.cwd = svim.fs.normalize(opts and opts.cwd or vim.uv.cwd() or '.') or nil
+              item.file = entry.file
+              item.line = entry.text
+              item.pos = { tonumber(start.line) + 1, tonumber(start.column) }
+            end
+          end,
+        },
+      }, ctx)
+    end,
+  }
+end)
 		end,
 	},
 }
