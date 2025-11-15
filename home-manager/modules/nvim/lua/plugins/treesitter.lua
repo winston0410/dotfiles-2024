283b77f06c90e4d5d077c1e8d3fec375c8097813
nobody <hugosum.dev@protonmail.com>
2025-11-15 21:33:45 +0000

    new treesitter

--- home-manager/modules/nvim/lua/plugins/treesitter.lua
+++ home-manager/modules/nvim/lua/plugins/treesitter.lua
@@ -29,10 +29,35 @@ return {
 		},
 	},
 	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		branch = "main",
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					lookahead = true,
+					selection_modes = {
+						["@parameter.outer"] = "v", -- charwise
+						["@function.outer"] = "V", -- linewise
+						["@class.outer"] = "<c-v>", -- blockwise
+					},
+					include_surrounding_whitespace = false,
+				},
+			})
+
+            local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
+
+            vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+            vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+
+            vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+            vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+            vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+            vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+		end,
+	},
+	{
 		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			-- { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
-		},
 		build = function()
 			vim.cmd("TSUpdate")
 		end,
