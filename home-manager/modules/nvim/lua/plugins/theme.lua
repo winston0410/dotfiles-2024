50d82321f9f2b036bef941f943d68e2f272495aa
Hugo Sum <hsum@trintech.com>
2025-12-11 09:44:24 +0000

    update theme

--- home-manager/modules/nvim/lua/plugins/theme.lua
+++ home-manager/modules/nvim/lua/plugins/theme.lua
@@ -5,19 +5,23 @@ vim.pack.add({
 	{ src = "https://github.com/folke/tokyonight.nvim" },
 	{ src = "https://github.com/e-ink-colorscheme/e-ink.nvim" },
 	{ src = "https://github.com/rose-pine/neovim" },
-	{ src = "https://github.com/thesimonho/kanagawa-paper.nvim" },
+	-- { src = "https://github.com/thesimonho/kanagawa-paper.nvim" },
 	{ src = "https://github.com/kyza0d/xeno.nvim" },
-	{ src = "https://github.com/AlexvZyl/nordic.nvim" },
+	-- { src = "https://github.com/AlexvZyl/nordic.nvim" },
 	{ src = "https://github.com/jnz/studio98" },
-	{ src = "https://github.com/nuvic/flexoki-nvim" },
-	{ src = "https://github.com/projekt0n/github-nvim-theme", version = vim.version.range("1.x") },
+	{ src = "https://github.com/Mofiqul/vscode.nvim" },
+    { src = "https://github.com/catppuccin/nvim" },
+	-- { src = "https://github.com/nuvic/flexoki-nvim" },
+	{ src = "https://github.com/miikanissi/modus-themes.nvim" },
+	-- { src = "https://github.com/projekt0n/github-nvim-theme", version = vim.version.range("1.x") },
 })
-
-vim.g.tokyonight_style = "moon"
-vim.cmd.colorscheme("tokyonight")
+require('vscode').setup({
+    style = "dark"
+})
 vim.opt.wildignore:append({
 	"tokyonight.lua",
 	"tokyonight-night.lua",
+	"tokyonight-storm.lua",
 	"tokyonight-day.lua",
 	"rose-pine.lua",
 	"rose-pine-main.lua",
@@ -25,35 +29,37 @@ vim.opt.wildignore:append({
 })
 vim.opt.wildignore:append({
 	"flexoki.lua",
-	-- don't look good in dark theme
-	"flexoki-moon.lua",
 })
 vim.opt.wildignore:append({
-	"github_dark.vim",
-	"github_dark_default.vim",
-	"github_dark_tritanopia.vim",
-	"github_dark_high_contrast.vim",
-	"github_dark_dimmed.vim",
-	"github_light.vim",
-	"github_light_default.vim",
-	"github_light_tritanopia.vim",
-	"github_light_colorblind.vim",
-	"github_light_high_contrast.vim",
+	"modus.lua",
 })
+vim.opt.wildignore:append({
+	"catppuccin.vim",
+	"catppuccin-latte.vim",
+	"catppuccin-frappe.vim",
+	"catppuccin-macchiato.vim",
+})
+-- vim.opt.wildignore:append({
+-- 	"github_dark.vim",
+-- 	"github_dark_default.vim",
+-- 	"github_dark_tritanopia.vim",
+-- 	"github_dark_high_contrast.vim",
+-- 	"github_dark_dimmed.vim",
+-- 	"github_light.vim",
+-- 	"github_light_default.vim",
+-- 	"github_light_tritanopia.vim",
+-- 	"github_light_colorblind.vim",
+-- 	"github_light_high_contrast.vim",
+-- })
 local xeno = require("xeno")
 xeno.config({
 	transparent = true,
 	contrast = 0.1,
 })
 
-xeno.new_theme("xeno-lilypad", {
-	base = "#1E1E1E",
-	accent = "#8CBE8C",
-	contrast = 0.1,
-})
-
 xeno.new_theme("xeno-golden-hour", {
 	base = "#11100f",
 	accent = "#FFCC33",
-	contrast = 0.1,
+	contrast = 0.2,
 })
+vim.cmd.colorscheme("modus_vivendi")
