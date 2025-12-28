00bce05394dcb9847aadc71ae69a19f3d84498c2
nobody <hugosum.dev@protonmail.com>
2025-12-28 20:22:41 +0000

    update

--- home-manager/modules/nvim/lua/custom/essential.lua
+++ home-manager/modules/nvim/lua/custom/essential.lua
@@ -6,13 +6,15 @@ vim.g.loaded_zip = 1
 vim.g.loaded_gzip = 1
 vim.g.loaded_matchit = 1
 vim.g.loaded_matchparen = 1
--- vim.g.loaded_remote_plugins = 1
-vim.g.loaded_shada_plugin = 0
+-- session is different with shada, and session is enough for us
+vim.g.loaded_shada_plugin = 1
 vim.g.loaded_spellfile_plugin = 1
 vim.g.loaded_tarPlugin = 1
 vim.g.loaded_2html_plugin = 1
 vim.g.loaded_tutor_mode_plugin = 1
--- vim.g.loaded_python3_provider = 0
+-- Disable remote plugins
+vim.g.loaded_remote_plugins = 1
+vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
