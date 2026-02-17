33c9beff35b873479227a3c6cae0207931433431
Hugo Sum <hsum@trintech.com>
2026-02-17 19:14:20 +0000

    improve session handling

--- home-manager/modules/nvim/lua/plugins/session-manager.lua
+++ home-manager/modules/nvim/lua/plugins/session-manager.lua
@@ -1,38 +1,43 @@
 local session_manager = require("custom.session-manager")
 local group = vim.api.nvim_create_augroup("SessionManager", { clear = true })
+
 vim.api.nvim_create_autocmd("User", {
-	pattern = "NvimReady",
-	once = true,
-	nested = true,
-	callback = function()
-		-- restore session after Nvim event loop has started, so the loading wouldn't block it
-		if vim.fn.argc() > 0 or vim.g.disable_session then
-			return
-		end
-		session_manager.load()
-	end,
+    pattern = "NvimReady",
+    once = true,
+    nested = true,
+    callback = function()
+        -- restore session after Nvim event loop has started, so the loading wouldn't block it
+        -- disable session, when nvim is used through pipe
+        if vim.fn.has('ttyin') == 0 then
+            return
+        end
+        if vim.fn.argc() > 0 or vim.g.disable_session then
+            return
+        end
+        session_manager.load()
+    end,
 })
 
 session_manager.setup({})
 
 local function save_qf()
-	local qflist = vim.fn.getqflist()
-	if #qflist == 0 then
-		return
-	end
+    local qflist = vim.fn.getqflist()
+    if #qflist == 0 then
+        return
+    end
 
-	local qf_session_dir = vim.fs.joinpath(session_manager.options.dir, "qf")
-	vim.fn.mkdir(qf_session_dir, "p")
+    local qf_session_dir = vim.fs.joinpath(session_manager.options.dir, "qf")
+    vim.fn.mkdir(qf_session_dir, "p")
 
-	for _, entry in ipairs(qflist) do
-		-- use filename instead of bufnr so it can be reloaded
-		entry.filename = vim.api.nvim_buf_get_name(entry.bufnr)
-		entry.bufnr = nil
-	end
+    for _, entry in ipairs(qflist) do
+        -- use filename instead of bufnr so it can be reloaded
+        entry.filename = vim.api.nvim_buf_get_name(entry.bufnr)
+        entry.bufnr = nil
+    end
 
-	local serialized_entries = vim.fn.json_encode(qflist)
+    local serialized_entries = vim.fn.json_encode(qflist)
 
-	vim.g.RestoreQuickFix = serialized_entries
+    vim.g.RestoreQuickFix = serialized_entries
 end
 
 ---@param buf_id integer
@@ -42,45 +47,48 @@ local function save_visual_range(buf_id)
 end
 
 vim.api.nvim_create_autocmd("VimLeavePre", {
-	group = group,
-	callback = function(ev)
-		if vim.g.disable_session then
-			return
-		end
-		save_qf()
+    group = group,
+    callback = function(ev)
+        if vim.fn.has('ttyin') == 0 then
+            return
+        end
+        if vim.g.disable_session then
+            return
+        end
+        save_qf()
         save_visual_range(ev.buf)
-		session_manager.save()
-	end,
+        session_manager.save()
+    end,
 })
 
 vim.api.nvim_create_autocmd("SessionLoadPost", {
-	group = group,
-	callback = function()
-		local qf_session_content = vim.g.RestoreQuickFix
-		if type(qf_session_content) ~= "string" then
-			return
-		end
-		local qf_session = vim.fn.json_decode(qf_session_content)
-		vim.fn.setqflist(qf_session, "r")
-	end,
+    group = group,
+    callback = function()
+        local qf_session_content = vim.g.RestoreQuickFix
+        if type(qf_session_content) ~= "string" then
+            return
+        end
+        local qf_session = vim.fn.json_decode(qf_session_content)
+        vim.fn.setqflist(qf_session, "r")
+    end,
 })
 
 vim.api.nvim_create_autocmd("SessionLoadPost", {
-	group = group,
-	callback = function()
-		for _, win in ipairs(vim.api.nvim_list_wins()) do
-			local buf = vim.api.nvim_win_get_buf(win)
-			local is_listed = vim.bo[buf].buflisted
-			if not is_listed then
-				vim.api.nvim_win_close(win, true)
-			end
-		end
-	end,
+    group = group,
+    callback = function()
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            local is_listed = vim.bo[buf].buflisted
+            if not is_listed then
+                vim.api.nvim_win_close(win, true)
+            end
+        end
+    end,
 })
 
 vim.api.nvim_create_autocmd("SessionLoadPost", {
-	group = group,
-	callback = function()
-		require("custom.dap").load_breakpoints()
-	end,
+    group = group,
+    callback = function()
+        require("custom.dap").load_breakpoints()
+    end,
 })
