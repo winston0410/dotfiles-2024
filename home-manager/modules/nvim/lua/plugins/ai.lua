116c1b958d74110591e3d19750d20adceaf8491f
Hugo Sum <hsum@trintech.com>
2026-03-31 09:58:17 +0100

    upgrade and make it work

--- home-manager/modules/nvim/lua/plugins/ai.lua
+++ home-manager/modules/nvim/lua/plugins/ai.lua
@@ -1,19 +1,7 @@
 vim.pack.add({
     -- pin to this version for now, as it is buggy afterwards
-	{ src = "https://github.com/NickvanDyke/opencode.nvim", version = "v0.1.0" },
+	{ src = "https://github.com/NickvanDyke/opencode.nvim", version = "v0.6.0" },
 }, { confirm = false })
-
-vim.g.opencode_opts = {
-	provider = {
-		enabled = "terminal",
-		wezterm = {
-			direction = "right",
-			percent = 33,
-		},
-        -- always start a new session, but optionally opt into an existing session
-        -- cmd = 'opencode --continue'
-	},
-}
 
 vim.keymap.set({ "n", "x" }, "<leader>p<leader>c", function()
 	require("opencode").select()
