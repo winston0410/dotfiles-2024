5b46c634708d70a4f11f9fcf376341e16dafa9d3
Hugo Sum <hsum@trintech.com>
2026-04-07 11:29:08 +0100

    clean up

--- home-manager/modules/nvim/lua/plugins/ai.lua
+++ home-manager/modules/nvim/lua/plugins/ai.lua
@@ -1,8 +1,12 @@
 vim.pack.add({
-    -- pin to this version for now, as it is buggy afterwards
+	-- pin to this version for now, as it is buggy afterwards
 	{ src = "https://github.com/NickvanDyke/opencode.nvim", version = "v0.6.0" },
 }, { confirm = false })
 
 vim.keymap.set({ "n", "x" }, "<leader>p<leader>c", function()
 	require("opencode").select()
 end, { desc = "Explore Opencode action" })
+
+vim.keymap.set({ "n", "x" }, "<leader>c", function()
+	return require("opencode").operator("@this ")
+end, { desc = "Add range to opencode", expr = true })
