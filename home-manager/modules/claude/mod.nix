0865044842d74d45a8adee06c4929726f7eb28bb
Hugo Sum <hsum@trintech.com>
2026-04-19 10:33:52 +0100

    add comment

--- home-manager/modules/claude/mod.nix
+++ home-manager/modules/claude/mod.nix
@@ -4,4 +4,9 @@
       source = ./keybindings.json;
     };
   };
+  # This doesn't work, because claude dump state and everything into this config dir
+  # home.sessionVariables = { CLAUDE_CONFIG_DIR = "${config.xdg.configHome}/claude"; };
+  # xdg.configFile = {
+  #   "claude/keybindings.json" = { source = ./keybindings.json; };
+  # };
 }
