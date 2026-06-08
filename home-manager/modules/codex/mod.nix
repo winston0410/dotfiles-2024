9bfc7fd43ccca60db6ee8d50f676b9c53b73c194
Hugo Sum <hsum@trintech.com>
2026-06-08 09:29:05 +0100

    pin

--- home-manager/modules/codex/mod.nix
+++ home-manager/modules/codex/mod.nix
@@ -1,6 +1,6 @@
 { inputs, lib, config, pkgs, unstable, ... }: {
   home.packages = [ unstable.codex unstable.codex-acp ];
-  home.file = {
-    ".codex/config.toml" = { source = ./config.toml; };
-  };
+  # home.file = {
+  #   ".codex/config.toml" = { source = ./config.toml; };
+  # };
 }
